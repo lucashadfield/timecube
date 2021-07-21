cf23c59d8ad10f94dddee1508e59d7bd35e6d32d
lucashadfield <lucas.hadfield@gmail.com>
2021-07-22 09:33:40 +1000

    refactored logic

--- timecube/timecube.py
+++ timecube/timecube.py
@@ -2,6 +2,7 @@ import asyncio
 import sys
 import time
 from dataclasses import dataclass
+from typing import Optional
 
 
 def cycle(one_cycle):
@@ -22,18 +23,18 @@ class TimeCube:
 
         self.interval_id = 0
         self.interval_task = None
-        self.interval_state_start = None
         self.saved_interval_duration = 0
-        self.state = 'run'
+
+        self.state_start = None
+        self.state = None
 
         self.summary_stats = {
             'work': 0,
             'break': 0,
             'longbreak': 0,
             'pause': 0,
-            'work_overflow': 0,
-            'break_overflow': 0,
-            'longbreak_overflow': 0,
+            'summary': 0,
+            'config': 0,
             'work_restarts': 0,
             'break_restarts': 0,
             'longbreak_restarts': 0,
@@ -42,9 +43,7 @@ class TimeCube:
         # each object is only created once - need to re-zero after they end
         work_interval = Interval('work', work_duration_s, 'knock')
         break_interval = Interval('break', break_duration_s, 'knock knock')
-        longbreak_interval = Interval(
-            'longbreak', longbreak_duration_s, 'knock knock knock'
-        )
+        longbreak_interval = Interval('longbreak', longbreak_duration_s, 'knock knock knock')
 
         self.cycle = []
         for n in range(n_work_loops - 1):
@@ -61,105 +60,87 @@ class TimeCube:
     def next_interval(self):
         return self.cycle[(self.interval_id + 1) % len(self.cycle)]
 
-    def print_summary_stats(self):
-        print({k: round(v) for k, v in self.summary_stats.items()})
+    def transition_state(self, new_state):
+        now = time.time()
+        duration = now - self.state_start
+        if isinstance(self.state, Interval):
+            self.summary_stats[self.state.type] += duration
+            self.saved_interval_duration = 0
+        else:
+            self.summary_stats[self.state] += duration
 
-    def log_interval_stats(self, actual_duration):
-        interval = self.current_interval
-        self.summary_stats[interval.type] += actual_duration
-        self.summary_stats[f'{interval.type}_overflow'] += max(
-            actual_duration - interval.duration, 0
-        )
+        self.state_start = now
+        self.state = new_state
         self.print_summary_stats()
 
-    def log_stats(self, key, increment):
-        self.summary_stats[key] += increment
-        # self.print_summary_stats()
+        return duration
 
-    async def start_interval_task(self):
-        duration = self.current_interval.duration - self.saved_interval_duration
-        print(f'starting {self.current_interval.type} for {duration} seconds')
-        await asyncio.sleep(duration)
-        print(f'finished {self.current_interval.type} ->  {self.next_interval.alarm}')
-        # do end of task changes here + alarm
+    def print_summary_stats(self):
+        print({k: round(v, 1) for k, v in self.summary_stats.items()})
+
+    async def start_interval_task(self, offset=0):
+        duration = self.current_interval.duration - offset
+        if duration > 0:
+            print(f'starting {self.current_interval.type} for {duration} seconds')
+            await asyncio.sleep(duration)
+            print(f'finished {self.current_interval.type} ->  {self.next_interval.alarm}')
+            # do end of task changes here + alarm
+
+    def start(self):
+        self.state_start = time.time()
+        self.state = self.current_interval
+        self.interval_task = asyncio.create_task(self.start_interval_task())
 
     def start_next_interval(self):
-        if self.state == 'run':
-            # logging
-            print('next interval')
-            interval_duration = time.time() - self.interval_state_start
-            self.log_interval_stats(interval_duration + self.saved_interval_duration)
-
-            # cancel current task (does nothing if not running)
+        if isinstance(self.state, Interval):
             self.interval_task.cancel()
 
-            # start next task
-            self.saved_interval_duration = 0
             self.interval_id += 1
-            self.interval_state_start = time.time()
+            self.transition_state(self.current_interval)
             self.interval_task = asyncio.create_task(self.start_interval_task())
 
-    def start_current_interval(self):
-        if self.state == 'run':
-            # start current task
-            if (
-                self.interval_state_start is not None
-            ):  # skip if it's the very first task
-                # logging
-                print('this interval')
-                interval_duration = time.time() - self.interval_state_start
-                self.log_interval_stats(
-                    interval_duration + self.saved_interval_duration
-                )
-                self.log_stats(f'{self.current_interval.type}_restarts', 1)
+    def restart_current_interval(self):
+        if isinstance(self.state, Interval):
+            self.interval_task.cancel()
 
-                # cancel current task (does nothing if not running)
-                self.interval_task.cancel()
-
-            # start current task
-            self.saved_interval_duration = 0
-            self.interval_state_start = time.time()
+            self.summary_stats[f'{self.current_interval.type}_restarts'] += 1
+            self.transition_state(self.current_interval)
             self.interval_task = asyncio.create_task(self.start_interval_task())
 
     def pause(self):
-        if self.state == 'run':
-            self.state = 'pause'
-
-            # logging
-            print('paused')
-            interval_duration = time.time() - self.interval_state_start
-
-            # cancel current task (does nothing if not running)
+        if self.state != 'pause':
             self.interval_task.cancel()
+            if isinstance(self.state, Interval):
+                self.saved_interval_duration += self.transition_state('pause')
+            else:
+                self.transition_state('pause')
 
-            # save the amount of time already spent on current interval
-            self.saved_interval_duration += interval_duration
-            # print(f'offset_duration = {self.offset_duration}')
-
-            # start timer for pause
-            self.interval_state_start = time.time()
-
-    def resume(self):
-        if self.state == 'pause':
-            self.state = 'run'
-
-            # logging
-            print('resuming')
-            pause_duration = time.time() - self.interval_state_start
-            self.log_stats('pause', pause_duration)
-
-            # restart the current task if it
-            self.interval_state_start = time.time()
-            if (
-                self.saved_interval_duration < self.cycle[self.interval_id].duration
-            ):  # time is still left on the current interval
-                self.interval_task = asyncio.create_task(self.start_interval_task())
-
-    def config(self):
-        pass
+            # do pause things
 
     def summary(self):
-        pass
+        if self.state != 'summary':
+            self.interval_task.cancel()
+            if isinstance(self.state, Interval):
+                self.saved_interval_duration += self.transition_state('summary')
+            else:
+                self.transition_state('summary')
+
+            # do summary things
+
+    def config(self):
+        if self.state != 'config':
+            self.interval_task.cancel()
+            if isinstance(self.state, Interval):
+                self.saved_interval_duration += self.transition_state('config')
+            else:
+                self.transition_state('config')
+
+            # do config things
+
+    def resume(self):
+        if self.state in ('pause', 'summary', 'config'):
+            self.transition_state(self.current_interval)
+            self.interval_task = asyncio.create_task(self.start_interval_task(self.saved_interval_duration))
 
 
 @dataclass
@@ -184,7 +165,7 @@ async def connect_stdin_stdout():
 async def main():
     timecube = TimeCube(3, 1, 2, 3)
 
-    timecube.start_current_interval()
+    timecube.start()
     print('this prints first')
 
     reader = await connect_stdin_stdout()
@@ -194,13 +175,15 @@ async def main():
         if res == b'l\n':
             timecube.start_next_interval()
         elif res == b'j\n':
-            timecube.start_current_interval()
+            timecube.restart_current_interval()
         elif res == b'k\n':
             timecube.pause()
         elif res == b'i\n':
             timecube.resume()
         elif res == b's\n':
-            print(timecube.summary_stats)
+            timecube.summary()
+        elif res == b'c\n':
+            timecube.config()
 
 
 if __name__ == '__main__':
